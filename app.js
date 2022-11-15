8e87e1ef8d71c1ff60e28026bee5dffafd7cdbd6
LukeB <lukebowers1@nhs.net>
2022-11-15 12:03:40 +0000

    task 4 get reviews

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require("express");
-const {getCategories} = require('./controllers/controller.js')
+const { getCategories, getReviews } = require("./controllers/controller.js");
 
 const app = express();
 
-
 app.get("/api/categories", getCategories);
 
-  // for 500's
-  app.use((err, req, res, next) => {
-    console.log(err)
-    res.status(500).send({msg: 'server error'})
-  })
+app.get("/api/reviews", getReviews);
+
+// for 500's
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "server error" });
+});
 
 module.exports = app;
