59ba66f74fa11d7ac15251fdf463109f36071bd9
LukeB <lukebowers1@nhs.net>
2022-11-18 11:16:38 +0000

    fix merge conflict

--- controllers/controller.js
+++ controllers/controller.js
@@ -47,32 +47,32 @@ exports.getCommentsByReviewId = (req, res, next) => {
 
 exports.postCommentByReviewId = (req, res, next) => {
   const { review_id } = req.params;
-  if(req.body.author === undefined && req.body.body === undefined){
-    res.status(400).send({ msg: "bad request" })
+  if (req.body.author === undefined && req.body.body === undefined) {
+    res.status(400).send({ msg: "bad request" });
+  } else {
+    const newReview = {
+      body: req.body.body,
+      author: req.body.username,
+      review_id: review_id,
+    };
+    insertCommentByReviewId(newReview)
+      .then((comment) => {
+        res.status(201).send({ comment });
+      })
+      .catch((err) => {
+        next(err);
+      });
   }
-  else{
-  const newReview = {
-    body: req.body.body,
-    author: req.body.username,
-    review_id: review_id,}
-  insertCommentByReviewId(newReview)
-    .then((comment) => {
-      res.status(201).send({ comment });
-    })
-    .catch((err) => {
-      next(err);
-    });
 
-}
-
-exports.patchReviewById = (req, res, next) => {
-  const {inc_votes} = req.body
-  const {review_id} = req.params
-  updateReviewById(review_id, inc_votes)
-  .then((review) => {
-    res.status(200).send({ review });
-  })
-  .catch((err) => {
-    next(err);
-  });
-}
+  exports.patchReviewById = (req, res, next) => {
+    const { inc_votes } = req.body;
+    const { review_id } = req.params;
+    updateReviewById(review_id, inc_votes)
+      .then((review) => {
+        res.status(200).send({ review });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  };
+};
